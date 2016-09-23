dbe3cb03f0f39707f627d79443132e82498a98ef
simondedman@gmail.com <simondedman@gmail.com>
2016-09-23 15:40:01 -0700

    removed all new manpage info: author, details, examples, from gbm.auto & valuemap

--- R/gbm.valuemap.R
+++ R/gbm.valuemap.R
@@ -33,8 +33,6 @@
 #'
 #' @export
 #'
-#' @author Simon Dedman, \email{simondedman@@gmail.com}
-#'
 #' @examples None
 gbm.valuemap <- function(
   dbase,  # data.frame to load. Expects Lon, Lat & data columns: predicted
