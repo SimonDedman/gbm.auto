431c1bd86029a043c11ce2baa8fb38ce3993f40a
simondedman@gmail.com <simondedman@gmail.com>
2016-10-05 15:30:43 -0700

    DESCRIPTION: v1.0.6 separate bin & gaus params release! sep bin/gaus runscript updated, problems may still be happening gbm.auto: overwritten with sep params version. params text word wrapped. tc/lr/bf descriptions updated to include list option. section numbers updated. tc/lr/bf list check & splitting code added ~L200. Bin & gaus loops separated. n & m counters used for all loops (n) & gaus only (m). report edited so sections omitted if not doing bin, order changed to be bin loops then gaus loops then all bin extras then all gaus extras. bin & gaus loops compressed from 5 cols per bin+gaus loop to 1 col per bin & 1 per gus loop. Need to remove comments L257. Bin & Gaus best score <- 0 moved before 3 loops so it's not overwritten each time. Progress printers updated to new looping style & list possibility. Delete comment L365. All references to lr (k) changed to actual lr value rather than k*100. gbm.auto backed up.

--- Gbm.auto_extras/gbmauto.sepbingaus.runscript.R
+++ Gbm.auto_extras/gbmauto.sepbingaus.runscript.R
@@ -1,50 +1,37 @@
-gbm.auto2(samples = Juveniles,
-          grids = mygrids,
-          expvar = c(4:10,14,16,20,24,28,36),
-          resvar = 43,
-          tc = c(2,13),
-          lr = c(0.01,0.005),
-          bf = 0.5,
-          mapshape = Crop_Map)
+mygrids <- gbm.auto::grids # load grids
+mysamples <- gbm.auto::samples # load samples
+setwd("/home/simon/Desktop/gbm temp/sepParamTestbench/")
+source('~/Dropbox/Galway/Analysis/R/gbm.auto/R/gbm.utils.R')
+library("shapefiles")
+Crop_Map <- read.shapefile("/home/simon/Desktop/gbm temp/CroppedMap/Crop_Map")
+source('~/Dropbox/Galway/Analysis/R/gbm.auto/Gbm.auto_extras/gbm.auto.binGausSepParams.R')
 
-gbm.auto2(samples = Juveniles,
-          grids = mygrids,
-          expvar = c(4:10,14,16,20,24,28,36),
-          resvar = 43,
-          tc = list(c(2,13),2),
-          lr = list(c(0.01,0.005),0.0001),
-          bf = list(c(0.5,0.7),0.5),
-          mapshape = Crop_Map)
+testlist <- list(c(1,2),c(3,4))
+testlist
+testlist[[1]]
+testlist[[2]]
+testved <- c(5,6)
+testved
+testlist2 <- list(c(7,8),9)
+testlist2
+testlist2[[1]]
+testlist2[[2]]
+testlist2 <- list(10,c(11,12))
+testlist2
+testlist2[[1]]
+testlist2[[2]]
+testlist3 <- list(c(1,2),c(3,4),c(5,6))
+testlist3
+testlist3[[1]]
+testlist3[[2]]
+testlist3[[3]]
+if (length(testlist3) > 2) {stop("Only 2 list items allowed: 1 bin 1 Gaus")}
 
-gbm.auto2(samples = Juveniles,
+gbm.auto2(samples = mysamples,
           grids = mygrids,
-          expvar = c(4:10,14,16,20,24,28,36),
-          resvar = 43,
-          tc = list(2, c(2,13)),
-          lr = list(0.01, c(0.01,0.005)),
-          bf = 0.5,
-          mapshape = Crop_Map, simp = FALSE, varint = FALSE, map = FALSE, BnW = FALSE, RSB = F, linesfiles = FALSE)
-
-gbm.auto2(samples = Juveniles,
-          +           grids = mygrids,
-          +           expvar = c(4:10,14,16,20,24,28,36),
-          +           resvar = 43,
-          +           tc = c(2,3),
-          +           lr = c(0.01,0.005),
-          +           bf = c(0.5,0.7),
-          +           mapshape = Crop_Map, simp = FALSE, varint = FALSE, map = FALSE, BnW = FALSE, RSB = F, linesfiles = FALSE, savegbm = F)
-[1] 2 3
-[1] 2 3
-[1] 0.010 0.005
-[1] 0.010 0.005
-[1] 0.5 0.7
-[1] 0.5 0.7
-
-gbm.auto2(samples = Juveniles,
-          grids = mygrids,
-          expvar = c(4:10,14,16,20,24,28,36),
-          resvar = 43,
+          expvar = c(4:10),
+          resvar = 11,
           tc = 2,
-          lr = list(c(0.01,0.02),0.001),
+          lr = list(c(0.05,0.04),0.03),
           bf = 0.5,
           mapshape = Crop_Map, simp = FALSE, varint = FALSE, map = FALSE, BnW = FALSE, RSB = F, linesfiles = FALSE, savegbm = F)
