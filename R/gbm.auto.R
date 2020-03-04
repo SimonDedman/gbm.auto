f0c8d596d5f79cd77cc431e5686c0a6aab16dfbd
simondedman@gmail.com <simondedman@gmail.com>
2020-03-04 05:50:24 -0800

    round MLEval value at end not per line, didn't work before, changed to digits = 5

--- R/gbm.auto.R
+++ R/gbm.auto.R
@@ -927,153 +927,153 @@ gbm.auto <- function(
         MLEvalLength <- 31
         # Improve descriptions####
         MLEval <- data.frame(Statistic = rep(NA, MLEvalLength),
-                                Description = rep(NA, MLEvalLength),
-                                Value = rep(NA, MLEvalLength))
+                             Description = rep(NA, MLEvalLength),
+                             Value = rep(NA, MLEvalLength))
         MLEval[1,] <- c("Presence",
-                           "n of presence data used",
-                           round(e@np), 3)
+                        "n of presence data used",
+                        e@np)
         MLEval[2,] <- c("Absence",
-                           "n of absence data used",
-                        round(e@na), 3)
+                        "n of absence data used",
+                        e@na)
         MLEval[3,] <- c("AUC",
-                           "Area under the receiver operator (ROC) curve",
-                        round(e@auc), 3)
+                        "Area under the receiver operator (ROC) curve",
+                        e@auc)
         if (length(e@pauc) == 0) e@pauc <- 0 # pauc may be missing, numeric(0), if so replace with 0
         MLEval[4,] <- c("pAUC",
-                           "p-value for the AUC (for the Wilcoxon test W statistic)",
-                        round(e@pauc), 3)
+                        "p-value for the AUC (for the Wilcoxon test W statistic)",
+                        e@pauc)
         MLEval[5,] <- c("Cor",
-                           "Correlation coefficient",
-                        round(e@cor[[1]]), 3)
+                        "Correlation coefficient",
+                        e@cor[[1]])
         MLEval[6,] <- c("cor",
-                           "p-value for correlation coefficient",
-                        round(e@pcor), 3)
+                        "p-value for correlation coefficient",
+                        e@pcor)
         # Steph Brodie's TSS which produces the same result as Allouche
         # -1 just makes the output range is 0:1 instead of 1:2 I think.
         # If so this means Sensitivity is e@TPR[which.max(e@TPR + e@TNR)], which doesn't include
         # (e@TPR + e@FNR) but it's a vector of 1s so is redundant. Same for Specificity
         MLEval[7,] <- c("TSS",
-                           "True Skill Statistic",
-                        round(max(e@TPR + e@TNR - 1)), 3)
+                        "True Skill Statistic",
+                        max(e@TPR + e@TNR - 1))
         # sensitivity: TP/(TP+FN)
         MLEval[8,] <- c("Sensitivity",
-                           "Sensitivity",
-                        round(e@TPR[which.max(e@TPR + e@TNR)]), 3)
+                        "Sensitivity",
+                        e@TPR[which.max(e@TPR + e@TNR)])
         # specificity: TN/(FP+TN)
         Specificity <- e@TNR[which.max(e@TPR + e@TNR)]
         MLEval[9,] <- c("Specificity",
-                           "Specificity",
-                        round(Specificity), 3)
+                        "Specificity",
+                        Specificity)
         # Accuracy: TP+TN / TP+TN+FP+FN true false positive negative.
         # TP+TN is just TSS + 1, TP+TN+FP+FN #Sums to 2, redundant
         MLEval[10,] <- c("Accuracy",
-                            "Accuracy",
-                         round((e@TPR[which.max(e@TPR + e@TNR)] + e@TNR[which.max(e@TPR + e@TNR)]) / 2), 3)
+                         "Accuracy",
+                         (e@TPR[which.max(e@TPR + e@TNR)] + e@TNR[which.max(e@TPR + e@TNR)]) / 2)
         # Precision: TP/TP+FP. Ignores true negatives. “X% of the predictions are right”
         Precision <- e@TPR[which.max(e@TPR + e@TNR)] / (e@TPR[which.max(e@TPR + e@TNR)] + e@FPR[which.max(e@TPR + e@TNR)])
         MLEval[11,] <- c("Precision",
-                            "X% of the predictions are right",
-                         round(Precision), 3)
+                         "X% of the predictions are right",
+                         Precision)
         # Recall: TP/TP+FN: “Y% of actually existing things are captured”.
         Recall <- e@TPR[which.max(e@TPR + e@TNR)] / (e@TPR[which.max(e@TPR + e@TNR)] + e@FNR[which.max(e@TPR + e@TNR)])
         MLEval[12,] <- c("Recall",
-                            "Y% of actually existing things are captured",
-                         round(Recall), 3)
+                         "Y% of actually existing things are captured",
+                         Recall)
         # https://www.corvil.com/kb/what-is-a-false-positive-rate
         # Allouche et al 2006:
         # overall accuracy: (TP+TN)/n
         # this seems like a weird metric since the numerator is 0:2 or 1:2 and the divisor could be tiny or huge
         MLEval[13,] <- c("OverallAccuracy",
-                            "Overall Accuracy",
-                         round((e@TPR[which.max(e@TPR + e@TNR)] + e@TNR[which.max(e@TPR + e@TNR)])/nrow(samples)), 3)
+                         "Overall Accuracy",
+                         (e@TPR[which.max(e@TPR + e@TNR)] + e@TNR[which.max(e@TPR + e@TNR)])/nrow(samples))
         # Balanced Accuracy, (Recall + Specificity) / 2
         MLEval[14,] <- c("BalancedAccuracy",
-                            "Balanced Accuracy",
-                         round((Recall + Specificity) / 2), 3)
+                         "Balanced Accuracy",
+                         (Recall + Specificity) / 2)
         # Number of samples. Useful to include in the list
         MLEval[15,] <- c("nSamples",
-                            "Number of samples",
-                         round(nrow(samples)), 3)
+                         "Number of samples",
+                         nrow(samples))
         # Balance: precision vs recall curve. Workhorses.
         # PxR/P+R = F score (P+R = harmonic mean).
         # F1 score: P & R are equally rated. This is the most common one. F1 score importance depends on the project.
         MLEval[16,] <- c("F1score",
-                            "P & R equally rated, score importance depends on project",
-                         round(2 * ((Precision * Recall) / (Precision + Recall))), 3)
+                         "P & R equally rated, score importance depends on project",
+                         2 * ((Precision * Recall) / (Precision + Recall)))
         # F2 score: weighted average of Precision & Recall
         MLEval[17,] <- c("F2score",
-                            "weighted average of P & R",
-                         round(5 * ((Precision * Recall) / (4 * Precision + Recall))), 3)
+                         "weighted average of P & R",
+                         5 * ((Precision * Recall) / (4 * Precision + Recall)))
         # Threshold which produces the best combo of TPR & TNR
         # t: vector of thresholds used to compute confusion matrices
         MLEval[18,] <- c("Threshold",
-                            "Threshold which produced best combo of TPR & TNR",
-                         round(e@t[which.max(e@TPR + e@TNR)]), 3)
+                         "Threshold which produced best combo of TPR & TNR",
+                         e@t[which.max(e@TPR + e@TNR)])
         # e@prevalence: Prevalence
         MLEval[19,] <- c("Prevalence",
-                            "Prevalence",
-                         round(e@prevalence[which.max(e@TPR + e@TNR)]), 3)
+                         "Prevalence",
+                         e@prevalence[which.max(e@TPR + e@TNR)])
         # e@ODP: Overall diagnostic power
         MLEval[20,] <- c("ODP",
-                            "Overall diagnostic power",
-                         round(e@ODP[which.max(e@TPR + e@TNR)]), 3)
+                         "Overall diagnostic power",
+                         e@ODP[which.max(e@TPR + e@TNR)])
         # e@CCR: Correct classification rate
         MLEval[21,] <- c("CCR",
-                            "Correct classification rate",
-                         round(e@CCR[which.max(e@TPR + e@TNR)]), 3)
+                         "Correct classification rate",
+                         e@CCR[which.max(e@TPR + e@TNR)])
         # e@TPR: True positive rate
         MLEval[22,] <- c("TPR",
-                            "True positive rate",
-                         round(e@TPR[which.max(e@TPR + e@TNR)]), 3)
+                         "True positive rate",
+                         e@TPR[which.max(e@TPR + e@TNR)])
         # e@TNR: True negative rate
         MLEval[23,] <- c("TNR",
-                            "True negative rate",
-                         round(e@TNR[which.max(e@TPR + e@TNR)]), 3)
+                         "True negative rate",
+                         e@TNR[which.max(e@TPR + e@TNR)])
         # e@FPR: False positive rate
         MLEval[24,] <- c("FPR",
-                            "False positive rate",
-                         round(e@FPR[which.max(e@TPR + e@TNR)]), 3)
+                         "False positive rate",
+                         e@FPR[which.max(e@TPR + e@TNR)])
         # e@FNR: False negative rate
         MLEval[25,] <- c("FNR",
-                            "False negative rate",
-                         round(e@FNR[which.max(e@TPR + e@TNR)]), 3)
+                         "False negative rate",
+                         e@FNR[which.max(e@TPR + e@TNR)])
         # e@PPP: Positive predictive power
         MLEval[26,] <- c("PPP",
-                            "Positive predictive power",
-                         round(e@PPP[which.max(e@TPR + e@TNR)]), 3)
+                         "Positive predictive power",
+                         e@PPP[which.max(e@TPR + e@TNR)])
         # e@NPP: Negative predictive power
         MLEval[27,] <- c("NPP",
-                            "Negative predictive power",
-                         round(e@NPP[which.max(e@TPR + e@TNR)]), 3)
+                         "Negative predictive power",
+                         e@NPP[which.max(e@TPR + e@TNR)])
         # e@MCR: Misclassification rate
         MLEval[28,] <- c("MCR",
-                            "Misclassification rate",
-                         round(e@MCR[which.max(e@TPR + e@TNR)]), 3)
+                         "Misclassification rate",
+                         e@MCR[which.max(e@TPR + e@TNR)])
         # e@OR: Odds-ratio
         MLEval[29,] <- c("OR",
-                            "Odds-ratio",
-                         round(e@OR[which.max(e@TPR + e@TNR)]), 3)
+                         "Odds-ratio",
+                         e@OR[which.max(e@TPR + e@TNR)])
         # e@kappa: Cohen's kappa
         MLEval[30,] <- c("kappa",
-                            "Cohen's kappa",
-                         round(e@kappa[which.max(e@TPR + e@TNR)]), 3)
+                         "Cohen's kappa",
+                         e@kappa[which.max(e@TPR + e@TNR)])
         # dev from calc.deviance from dismo
         MLEval[31,] <- c("dev",
                          "deviance from 2 vecs, obs & pred vals",
-                         round(dev), 3)
+                         dev)
       } # close if any bernoulli at top of TSS section
+        MLEval$Value <- round(MLEval$Value, digits = 5)
+        write.csv(MLEval, row.names = FALSE, na = "", file = paste0("./", names(samples[i]), "/MLEvalMetrics.csv"))
 
-      write.csv(MLEval, row.names = FALSE, na = "", file = paste0("./", names(samples[i]), "/MLEvalMetrics.csv"))
-
-      evalmetrics <- c("ROC", "kappa", "prevalence", "TPR", "TNR", "FPR", "FNR", "CCR", "PPP", "NPP", "MCR", "OR")
-      for (s in evalmetrics) {
-        png(filename = paste0("./",names(samples[i]),"/Eval_", s, ".png"))
-        plot(e, s)
-        dev.off()
-      }
-      # can do calc.deviance for gaus also, ditto poisson
-      if (alerts) beep(2) # progress printer, right aligned for visibility
-      print(paste0("XXXXXXXXXXXXXXXXXXXXXXXXXXXX     Evaluation Metrics Processed     XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"))
+        evalmetrics <- c("ROC", "kappa", "prevalence", "TPR", "TNR", "FPR", "FNR", "CCR", "PPP", "NPP", "MCR", "OR")
+        for (s in evalmetrics) {
+          png(filename = paste0("./",names(samples[i]),"/Eval_", s, ".png"))
+          plot(e, s)
+          dev.off()
+        }
+        # can do calc.deviance for gaus also, ditto poisson
+        if (alerts) beep(2) # progress printer, right aligned for visibility
+        print(paste0("XXXXXXXXXXXXXXXXXXXXXXXXXXXX     Evaluation Metrics Processed     XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"))
       } # close if MLEvaluate
 
       if (alerts) beep(2) # progress printer, right aligned for visibility
