82ddc1b6cb8272b246d5f1f9fd6aff825c6d2795
Simon Dedman <simondedman@gmail.com>
2021-02-08 12:41:58 -0800

    basemap tweak for namespace bugfix attempt

--- R/gbm.basemap.R
+++ R/gbm.basemap.R
@@ -177,6 +177,6 @@ gbm.basemap <- function(bounds = NULL, # region to crop to: c(xmin,xmax,ymin,yma
   dir.create("CroppedMap") # create conservation maps directory
   setwd("CroppedMap")
   st_write(cropshp, dsn = paste0(savename, ".shp"))
-  cropshp <- shapefiles::read.shapefile(savename) # read it back in with read.shapefile which results in the expected format for draw.shape in mapplots, used in gbm.map
+  cropshp <- read.shapefile(savename) # read it back in with read.shapefile which results in the expected format for draw.shape in mapplots, used in gbm.map # shapefiles::
   print(paste("World map cropped and saved successfully"))
   return(cropshp)}
