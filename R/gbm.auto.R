526ba6f22c15ee95f98fc0d203f98a7de1f77400
Simon Dedman <simondedman@gmail.com>
2022-09-01 22:21:48 -0700

    auto move grv.csv to species folder not parent

--- R/gbm.auto.R
+++ R/gbm.auto.R
@@ -564,7 +564,7 @@ gbm.auto <- function(
           for (l in bfgaus) {   # list permutations of bag fraction allowed
             ####7. Gaussian BRT####
             print(paste0("Running ", fam2, " BRT, tc=",j,", lr=",k,", bf=",l))
-            write.csv(x = grv_yes[,grvcol], file = "grv.csv", row.names = FALSE)
+            write.csv(x = grv_yes[,grvcol], file = paste0("./",names(samples[i]),"/grv.csv"), row.names = FALSE)
             assign(paste0("Gaus_BRT",".tc",j,".lr",k,".bf",l),
                    gbm.step.sd(data = grv_yes,
                                gbm.x = expvar,
